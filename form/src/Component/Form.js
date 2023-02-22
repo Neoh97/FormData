8b7a47c5a32898a4424fe09e4df41891e10e23c2
Neoh97 <neohveil@gmail.com>
2023-02-22 10:20:41 -0500

    second commit

--- form/src/Component/Form.js
+++ form/src/Component/Form.js
@@ -18,7 +18,7 @@ const FormIncription = () =>{
 
         if(values.every(v => v !== "")){
 
-            axios.post("http://localhost:3001/api/inscription", dataForm); 
+            axios.post("formdata-production.up.railway.app/api/inscription", dataForm); 
             // JSON.stringify(dataForm)
            document.getElementById("myForm").reset();
            alert("¡Enviado!");
