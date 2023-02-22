281997d3d1e915f3895f80592c586cf5823689d2
Neoh97 <neohveil@gmail.com>
2023-02-22 11:57:25 -0500

    three commit

--- api/app.js
+++ api/app.js
@@ -10,6 +10,23 @@ server.use(cors()); // manejo de error de origen crusado
 server.use(morgan("tiny")); // historial de peticiones 
 server.use(express.json()); //manejo de info en json
 // server.use(require("./hook/contentRangeHook")); 
+
+
+
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// app.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+// app.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // or `http://localhost:${FRONT}`// update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
     //
 server.use("/api/inscription",require("./routers/DataForm")); //uso de rutas 
     //server.use("/api/inscription", sendEmail);
